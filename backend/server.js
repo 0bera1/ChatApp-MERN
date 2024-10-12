4d8bbdde6ecb8f01d3e8f305af635d594a7d21de
Bera <102685755+0bera1@users.noreply.github.com>
2024-10-13 01:28:06 +0300

    Port parts updated

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
-
+const dotenv = require("dotenv");
 const app = express();
 
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+
 app.get("/", (req, res) => {
-    // root route http://localhost:5001/    
-    res.send("Hello World");
+  // root route http://localhost:5001/
+  res.send("Hello World !!!");
 });
 
-app.listen(5001, () => console.log("Server running on port 5001"));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
