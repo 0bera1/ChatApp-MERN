e988de40835f410eea10f630d5a79d8f7bc805ce
Bera <102685755+0bera1@users.noreply.github.com>
2024-10-29 16:30:10 +0300

    deployed

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
@@ -8,23 +9,33 @@ import userRoutes from "./routes/user.routes.js";
 import { app, server } from "./socket/socket.js";
 
 // const app = express(); // create express app
+dotenv.config(); // read .env file
+
+
+const __dirname = path.resolve();
+
 
 const PORT = process.env.PORT || 5001; // get port from .env file
-dotenv.config(); // read .env file
+
+
 
 app.use(express.json()); // parse json body of requests from req.body (bu kod ile gelen isteklerin body kısmındaki json verileri okunabilir (req.body den))
 app.use(cookieParser()); // parse cookies from requests
-
-// app.get("/", (req, res) => {
-//   // root route handler
-//   res.send("Hello World !!!");
-// });
-
 app.use("/api/auth", authRoutes); // /api/auth ile başlayan istekler authRoutes dosyasına yönlendirilecek
 // authRoutes burada şunu yapıyor: /api/auth/signup, /api/auth/login, /api/auth/logout
 // yani auth ile ilgili fonksiyonları çağırıyor.
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+// bu satır ile frontend/dist klasöründeki dosyaları sunucuya yüklemiş oluyoruz.
+app.use(express.static(path.join(__dirname, "/frontend/dist"))); // serve static files from frontend/dist folder
+
+//   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html")) satırı şunu yapıyor: frontend/dist klasöründeki index.html dosyasını sunucuya yükleyip gösteriyor.
+// app.get("/", (req, res) ise root route handler. Yani root route a geldiğinde index.html dosyasını gösteriyor.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 server.listen(PORT, () => {
   // start server
   connectDB(); // connect to database
